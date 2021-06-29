c278bd0917302ac8f42db74194042ff40f7a9827
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2021-06-29 12:13:27 +0530

    Update HEAP create del insert sort.cpp

--- heap and hashing/HEAP create del insert sort.cpp
+++ heap and hashing/HEAP create del insert sort.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insert(int a[],int n)
+{
+ int i=n,temp;
+  temp=a[i];
+  while(i>1 && temp>a[i/2])
+  {
+    a[i]=a[i]/2;
+    i=i/2;
+  }
+  a[i]=temp;
+}
+
+int main()
+{
+  a=[0,10,20,30,35,5,40,35];
+  int i;
+  for(i=2;i<7;i++)
+    insert(a,i);
+  for(i=1;i<=7;i++)
+    cout<<a[i]<<" ";
+}
 
